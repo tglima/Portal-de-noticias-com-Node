1d9934819aee6aceb1e47308905857731765e42b
tglima <thg.limadesousa@gmail.com>
2017-11-03 19:38:22 -0200

    Instalado o Express Validator e aplicado no projeto.

--- app/models/NoticiasDAO.js
+++ app/models/NoticiasDAO.js
@@ -1,15 +1,16 @@
 function NoticiasDAO(connection){
 	this._connection =  connection;
 }NoticiasDAO.prototype.getNoticias = function(callback){
-		this._connection.query('select * from tb_noticias', callback);
+	this._connection.query('select * from tb_noticias', callback);
 	}
 
 NoticiasDAO.prototype.getNoticia =	function(callback){
-		this._connection.query('select * from tb_noticias where id_noticia = 2', callback);
+	this._connection.query('select * from tb_noticias where id_noticia = 2', callback);
 	}
 
 NoticiasDAO.prototype.salvarNoticia = 	function(noticia, callback){
-		this._connection.query('INSERT INTO tb_noticias SET ?', noticia, callback);
+	console.log(noticia);
+	this._connection.query('INSERT INTO tb_noticias SET ?', noticia, callback);
 	}
 
 module.exports = function(){
