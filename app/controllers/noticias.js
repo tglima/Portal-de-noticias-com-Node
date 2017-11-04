21425009541e90141b3077f0a012a2729909ef3f
tglima <thg.limadesousa@gmail.com>
2017-11-04 18:47:58 -0200

    Refatoração para que as views se tornem dinâmicas

--- app/controllers/noticias.js
+++ app/controllers/noticias.js
@@ -9,7 +9,9 @@ module.exports.noticias = function(application, req, res){
 module.exports.noticia = function(application, req, res){
     var connection = application.config.dbConnection();
     var noticiasModel = new application.app.models.NoticiasDAO(connection);
-    noticiasModel.getNoticia(function(error, result){
+    var id_noticia = req.query;
+
+    noticiasModel.getNoticia(id_noticia, function(error, result){
         res.render('noticias/noticia', { noticia : result });
     });	
 }
--- app/models/NoticiasDAO.js
+++ app/models/NoticiasDAO.js
@@ -1,17 +1,21 @@
 function NoticiasDAO(connection){
 	this._connection =  connection;
 }NoticiasDAO.prototype.getNoticias = function(callback){
-	this._connection.query('select * from tb_noticias', callback);
+	this._connection.query('select * from tb_noticias order by data_criacao desc', callback);
 	}
 
-NoticiasDAO.prototype.getNoticia =	function(callback){
-	this._connection.query('select * from tb_noticias where id_noticia = 2', callback);
+NoticiasDAO.prototype.getNoticia =	function(id_noticia, callback){
+	this._connection.query('select * from tb_noticias where id_noticia = ' + id_noticia.id_noticia +'', callback);
 	}
 
 NoticiasDAO.prototype.salvarNoticia = 	function(noticia, callback){
 	console.log(noticia);
 	this._connection.query('INSERT INTO tb_noticias SET ?', noticia, callback);
 	}
+
+NoticiasDAO.prototype.get5UltimasNoticias = function(callback){
+	this._connection.query('SELECT * FROM tb_noticias ORDER BY data_criacao desc limit 5', callback);
+}
 
 module.exports = function(){
 	return NoticiasDAO;
